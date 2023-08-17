3f67e6ceb6c3e64f94d86aea589c7e1e14ee0e2a
SandorStefan62 <80056950+SandorStefan62@users.noreply.github.com>
2023-08-17 16:28:40 +0300

    Create Weather App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
 import { useEffect, FC } from 'react'
 import './App.css'
+import WeatherBanner from './components/WeatherBanner/WeatherBanner'
 
 const App: FC = () => {
 	useEffect(() => {
 		console.log('kurwa')
 	}, [])
 
-	return <></>
+	return (
+		<>
+			<WeatherBanner></WeatherBanner>
+		</>
+	)
 }
 
 export default App
